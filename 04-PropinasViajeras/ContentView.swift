cfa8950b9cf9b17d29e5109640653b233231c284
Valentin Sanchez <elzero_2@hotmail.com>
2020-07-27 12:15:11 +0200

    Initial Commit

---
+++ 04-PropinasViajeras/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  04-PropinasViajeras
+//
+//  Created by Valentín Sánchez Campañón on 27/07/2020.
+//  Copyright © 2020 Valentín Sánchez Campañón. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
